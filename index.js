3a94ccb3f08c10ab2542c74a80e89f5a137c2087
klojtas <do-not-reply@example.com>
2025-07-02 22:45:22 +0200

    changed output of the messages

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const core = require('@actions/core');
 
-try {
-  const startDate = new Date(core.getInput('start-date'));
-  const daysTillError = parseInt(core.getInput('days-till-error'), 10);
-  const warningMessage = core.getInput('warning-message');
-  const errorMessage = core.getInput('error-message');
+async function run() {
+  try {
+    const startDate = new Date(core.getInput('start-date'));
+    const daysTillError = parseInt(core.getInput('days-till-error'), 10);
+    const warningMessage = core.getInput('warning-message');
+    const errorMessage = core.getInput('error-message');
 
-  if (isNaN(startDate.getTime())) {
-    core.setFailed('Invalid start-date. Please provide a date in YYYY-MM-DD format.');
-    return;
+    if (isNaN(startDate.getTime())) {
+      core.setFailed('Invalid start-date. Please provide a date in YYYY-MM-DD format.');
+      return;
+    }
+
+    if (isNaN(daysTillError)) {
+      core.setFailed('Invalid days-till-error. Please provide an integer.');
+      return;
+    }
+
+    const currentDate = new Date();
+    const errorDate = new Date(startDate);
+    errorDate.setDate(startDate.getDate() + daysTillError);
+
+    if (currentDate >= errorDate) {
+      core.setFailed(errorMessage);
+      core.summary.addRaw(errorMessage).write();
+    } else {
+      core.warning(warningMessage);
+      core.setOutput('deprecation-message', warningMessage);
+      core.summary.addRaw(warningMessage).write();
+    }
+  } catch (error) {
+    core.setFailed(error.message);
   }
-
-  if (isNaN(daysTillError)) {
-    core.setFailed('Invalid days-till-error. Please provide an integer.');
-    return;
-  }
-
-  const currentDate = new Date();
-  const errorDate = new Date(startDate);
-  errorDate.setDate(startDate.getDate() + daysTillError);
-
-  if (currentDate >= errorDate) {
-    core.setFailed(`ERROR: ${errorMessage}`);
-  } else {
-    core.warning(`WARNING: ${warningMessage}`);
-    core.setOutput('deprecation-message', `WARNING: ${warningMessage}`);
-  }
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+run();
